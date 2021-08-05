f435cfb4af3e45fc8a4bd0e113e6f61a7222f5da
Marco Daniël Leguijt <m.leguijt@outlook.com>
2021-08-05 14:29:35 +0200

    'type' => 'FieldLabelPosEnum' doesn't exist.

--- src/type/object/class-field-type.php
+++ src/type/object/class-field-type.php
@@ -181,7 +181,7 @@ class Field_Type {
 				'description' => __( 'The field is required?', 'wp-graphql-ninja-forms' ),
 			],
 			'labelPos'               => [
-				'type'        => 'FieldLabelPosEnum',
+				'type'        => 'String',
 				'description' => __( 'Position of the label', 'wp-graphql-ninja-forms' ),
 			],
 			'personallyIdentifiable' => [
