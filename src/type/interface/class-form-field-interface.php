b1635844c22b9d03cd0db7fb9c7bbe911d2ef9a9
Marco Daniël Leguijt <m.leguijt@outlook.com>
2021-08-05 14:30:04 +0200

    had typo

--- src/type/interface/class-form-field-interface.php
+++ src/type/interface/class-form-field-interface.php
@@ -83,7 +83,7 @@ class Form_Field_Interface {
 						'description' => __( 'The field is required?', 'wp-graphql-ninja-forms' ),
 					],
 					'labelPos'               => [
-						FieldLabelPosEnum						'type'        => 'String',
+						'type'        => 'String',
 						'description' => __( 'Position of the label', 'wp-graphql-ninja-forms' ),
 					],
 					'personallyIdentifiable' => [
